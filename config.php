7dbb389480d83186614e9f9e0a74bb5c38ff19a6
6131548 <39148373+6131548@users.noreply.github.com>
2018-09-28 09:42:56 +0800

    商户APPID，MCH_ID KEY存储
    商户APPID，MCH_ID KEY存储，请改成自己真实的

---
+++ config.php
@@ -0,0 +1,11 @@
+
+return [
+
+    'wechat'=>[
+        'key'=>'2kiP0u1jzcqVc69z0s4f792Yke4vr5rR',//支付的签名key(在商户平台API安全按钮中获取)
+        'appid'=>'wx3a4485873dde06d5', //appid
+        'mch_id'=>'1400198002',//商户号 
+        
+    ],
+   
+];
